65d0bb2f0378e25cd35f6bfdbf4876ce9464ce87
Oyetola David <tolafandpro@gmail.com>
2023-01-20 08:16:43 +0100

    mobile responsiveness optimised

--- src/components/TrendingCoin.js
+++ src/components/TrendingCoin.js
@@ -10,29 +10,28 @@ const TrendingCoin = ({data}) => {
    }
 
   return (
-    <div className='w-[40%]  mb-12 rounded-lg
-     p-4 relative cursor-pointer hover:bg-opacity-40 hover: bg-gray-200'
+    <div className='lg:w-[40%] sm:w-[60%] w-[80%] bg-gray-200 mb-12 last:mb-0 rounded-lg p-4 relative cursor-pointer hover:bg-gray-100 hover:bg-opacity-40'
      onClick={() => getCoinDetails(data.id)}
     >{data ? 
         <>
-            <h3 className='txt-base flex items-center my-0.5'>
+            <h3 className='text-base flex items-center my-0.5'>
             <span className='text-gray-100 capitalize'>name:&nbsp;</span>
             <span className='text-Pneon-50'>{ data.name}</span>
             <img className='w-[1.5rem] h-[1.5rem] mx-2 rounded-full' src={data.small} alt={data.name} />
         </h3>
 
-        <h3 className='txt-base flex items-center my-0.5'>
+        <h3 className='text-base flex items-center my-0.5'>
             <span className='text-gray-100 capitalize'>market cap rank:&nbsp;</span>
             <span className='text-Pneon-50'>{ data.market_cap_rank}</span>
         </h3>
-        <h3 className='txt-base flex items-center my-0.5'>
+        <h3 className='text-base flex items-center my-0.5'>
             <span className='text-gray-100 capitalize'>price (in btc):&nbsp;</span>
             <span className='text-Pneon-50'>
             {
                 new Intl.NumberFormat("en-NG", {style: "currency",  currency:"btc", maximumSignificantDigits: 5,}).format(data.price_btc)
             }</span>
         </h3>
-        <h3 className='txt-base flex items-center my-0.5'>
+        <h3 className='text-base flex items-center my-0.5'>
             <span className='text-gray-100 capitalize'>score:&nbsp;</span>
             <span className='text-Pneon-50'>{ data.score}</span>
         </h3>
